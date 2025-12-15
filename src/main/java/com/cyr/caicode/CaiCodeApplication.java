da191137704fbbf403b1857adea2829ec6b082f3
caiyu <975131701@qq.com>
2025-12-15 22:16:59 +0800

    用户模块后端

--- src/main/java/com/cyr/caicode/CaiCodeApplication.java
+++ src/main/java/com/cyr/caicode/CaiCodeApplication.java
@@ -1,9 +1,11 @@
 package com.cyr.caicode;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@MapperScan("com.cyr.caicode.mapper")
 public class CaiCodeApplication {
 
     public static void main(String[] args) {
---
+++ src/main/java/com/cyr/caicode/mapper/UserMapper.java
@@ -0,0 +1,13 @@
+package com.cyr.caicode.mapper;
+
+import com.mybatisflex.core.BaseMapper;
+import com.cyr.caicode.model.entity.User;
+
+/**
+ * 用户 映射层。
+ *
+ * @author <a href="https://github.com/YRcai13">cyr</a>
+ */
+public interface UserMapper extends BaseMapper<User> {
+
+}
